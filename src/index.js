3656505b9d9f0c61c7da8fa8b81d7c3de0e80cbf
AleksandraJarz <jarzebska.aleksandraa@gmail.com>
2023-12-18 20:54:51 +0100

    feat: commit

--- src/index.js
+++ src/index.js
@@ -104,20 +104,6 @@ function photosRenderer(data) {
     )
     .join('');
   gallery.insertAdjacentHTML('beforeend', html);
-
-  if (page === 1) {
-    lightbox = new SimpleLightbox('.gallery a');
-  } else {
-    lightbox.refresh();
-    const { height: cardHeight } = document
-      .querySelector('.gallery')
-      .firstElementChild.getBoundingClientRect();
-
-    window.scrollBy({
-      top: cardHeight * 2,
-      behavior: 'smooth',
-    });
-  }
 }
 
 //nasłuchiwanie kliknięcia na submit
